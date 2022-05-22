ab216f38ddc87f8d8fa1d87b229636ad0bdf0cf9
Anastasiia <anastasiiasorina@gmail.com>
2022-05-22 16:18:25 +0200

    todo

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import React, { useState } from 'react';
-import List from './components/List/List.jsx';
 import listSvg from './assets/img/list.png';
-import AddList from './components/AddList/AddList.jsx';
 import DB from './assets/db.json';
-import  './index.scss';
+import './index.scss';
 import './App.scss';
-import Tasks from './components/Tasks/Tasks.jsx'
+import axios from 'axios';
+
+import {List, AddList, Tasks } from './components';
+
 function App() {
+ axios.get('htpp://localhost:3001/lists?_expand=color').then(({ data })) => {
+   console.log(data);
+ });
+
   const [lists, setLists] = useState(
     DB.lists.map(item => {
       item.color = DB.colors.filter(color => color.id === item.colorId)[0].name;
       return item;
-   })
+    })
   );
-const onAddList = (obj) => {
-  const newList = [...lists, obj];
-  setLists(newList);
-};
+  const onAddList = (obj) => {
+    const newList = [...lists, obj];
+    setLists(newList);
+  };
 
   return (
     <div className="todo">
@@ -27,11 +32,11 @@ const onAddList = (obj) => {
             name: 'All tasks',
           }
         ]} />
-        <List items={lists} onRemove={(list) => { console.log(list);}} isRemovable />
-        <AddList onAdd ={onAddList} colors={DB.colors} />
-    </div>
+        <List items={lists} onRemove={(list) => { console.log(list); }} isRemovable />
+        <AddList onAdd={onAddList} colors={DB.colors} />
+      </div>
       <div className="todo__tasks">
-        <Tasks /> 
+        <Tasks />
       </div>
     </div>
   );
